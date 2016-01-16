c5cf4e3cbe1cf1bea479bea8aaebf88e60581d63
rasko-lazic <raskolazic@gmail.com>
2016-01-16 11:06:47 +0100

    pc update

---
+++ html/learning-php/index.php
@@ -0,0 +1,37 @@
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>My projects</title>
+  <meta name="description" content="My projects">
+  <meta name="author" content="Rasko Lazic">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+ 
+</head>
+<h1>Lista projekata</h1>
+<body>
+
+<?php
+
+//UBACUJE U PROMENLJIVU DIR SVE FAJLOVE I DIREKTORIJUME SA PATHA
+$dir=scandir('.');
+echo "<pre>";                                                                   //defines preformatted text
+for($brojac=2;$brojac<count($dir);$brojac++) {
+    if($dir[$brojac]!="index.php") {
+        ob_start();
+        include $dir[$brojac];
+        ob_end_clean();
+        //echo $dir[$i];
+        echo "<a href=/learning-php/".$dir[$brojac].">".$title."</a><br>";
+    }
+}
+?>
+    
+</body>
+</html>
---
+++ html/learning-php/string.php
@@ -0,0 +1,42 @@
+<html>
+    <body>
+        <form method="post">
+            Input your string: <input type="text" name="string1" placeholder="username"><br>
+            Input your string: <input type="password" name="string2" placeholder="password"><br>
+            <input type="submit">
+        </form>
+        <form action="string.php" method="get">
+            Input your string: <input type="text" name="string3"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
+
+
+<?php
+/*razlike izmedju _POST i _GET[in url]
+ * 
+ * string[] automatski rasporedjuje u array sa odvojenim indeksima
+ */
+
+$title="String input tests";
+//FORMATIRA IZLAZ
+//echo "<pre>"; 
+//PRINT_R ZA OUTPUT NIZOVA                                                                
+//print_r($_POST);                                                              
+//echo $_POST["string1"];                       
+//VADJENJE ODREDJENIH ELEMENATA STRINGA
+echo "First three letters: ".substr($_POST["string1"], 0, 3)."<br><br>";        
+echo "Username: ".$_POST["string1"]."<br>";
+//POGLEDATI PHP FORM VALIDATION
+echo "Password: ";
+if($_POST["string2"]!="") {
+    //MD5 ENKRIPCIJA
+    echo md5($_POST["string2"]);                                                
+    }
+//FLUSH-UJE $_POST
+$_POST=array();                                         
+echo "<br> _GET string: ".$_GET["string3"];
+
+echo'<br><br><a href="/learning-php/index.php">Back</a>';
+?>
