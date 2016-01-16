8f6ae246080c1de49f8635cdb5fdd4d6fc2f71a1
rasko-lazic <raskolazic@gmail.com>
2016-01-16 11:25:58 +0100

    pc update 2

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,10 @@ sudo apt-get install -y curl
 #Apache2
 sudo apt-get install -y apache2
 a2enmod rewrite
-<<<<<<< HEAD
 #sudo rm -v /var/www
 #sudo ln -fs /vagrant/ /var/www
 sudo rm -rf /var/www
 sudo ln -s /vagrant/ /var/www
-=======
-sudo rm -v /var/www
-sudo ln -fs /vagrant/ /var/www
->>>>>>> f72edd4d659254a2af280e53c26a033d89348df4
 #sudo cp /vagrant/setup/apache-default.conf /etc/apache2/sites-available/default.conf
 #sudo a2ensite default
 #sudo a2dissite 000-default
@@ -34,7 +29,6 @@ sudo apt-get -y install php5-gd
 sudo apt-get -y install php5-curl
 #RESTART Apache2
 sudo service apache2 restart
-<<<<<<< HEAD
 #postgresql
 sudo apt-get install postgresql-client
 sudo apt-get install postgresql 
@@ -42,10 +36,3 @@ sudo apt-get install postgresql-contrib
 #composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 #composer update
-=======
-#postgres
-sudo apt-get install postgresql-client
-#composer
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-#composer update
->>>>>>> f72edd4d659254a2af280e53c26a033d89348df4
